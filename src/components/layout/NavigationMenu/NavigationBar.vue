cc3bd0f5fe157d51f014fe29fbb6bdcf83ed7ad7
Song Yang <songyang1218@gmail.com>
2022-06-19 23:56:55 -0400

    ✨ Desktop navigation bar

---
+++ src/components/layout/NavigationMenu/NavigationBar.vue
@@ -0,0 +1,116 @@
+<template>
+  <!-- Large nav for desktop -->
+  <nav class="desktop-only">
+    <ul>
+      <li style="margin-left: 0">
+        <figure>
+          <img :src="require('@/assets/images/layout/logo.png')" alt="Logo of the Universal Party">
+        </figure>
+      </li>
+      <li>
+        <router-link to="/platform" class="no-underscore">
+          <h1>Platform</h1>
+        </router-link>
+      </li>
+      <li>
+        <router-link to="/people" class="no-underscore">
+          <h1>Candidates 2022</h1>
+        </router-link>
+      </li>
+      <li>
+        <router-link to="/donate-volunteer" class="no-underscore">
+          <h1>Donate</h1>
+        </router-link>
+      </li>
+      <li>
+        <router-link to="/contact" class="no-underscore">
+          <h1>Contact</h1>
+        </router-link>
+      </li>
+      <li>
+        <router-link to="/about" class="no-underscore">
+          <h1>About</h1>
+        </router-link>
+      </li>
+      <li>
+        <h1>Lang</h1>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<style scoped lang="scss">
+.no-underscore {
+  text-decoration: none;
+}
+
+nav.desktop-only {
+
+  // display only when on desktop
+  @media only screen and (max-width: 796px) {
+    display: none;
+  }
+
+  // rectangular background
+  position: fixed;
+  width: 100%;
+  height: 60px;
+  overflow: hidden;
+  margin: 0;
+  left: 0;
+  top: 0;
+
+  background: #FFFFFF;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+
+  ul {
+    padding: 0;
+    margin: 0;
+    // no bullet
+    list-style-type: none;
+
+    // center the list elements
+    display: flex;
+  }
+
+  // horizontal
+  li {
+    float: left;
+
+    // center the list elements
+    margin: auto;
+  }
+
+  // font for the links
+  h1 {
+    font-family: 'Open Sans';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 24px;
+    line-height: 49px;
+    display: flex;
+    align-items: center;
+    text-align: center;
+
+    color: #000000;
+
+    margin: auto;
+    margin-left: 10px;
+    margin-right: 10px;
+  }
+
+  figure {
+    margin: auto;
+  }
+}
+</style>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({
+  components: {
+  },
+})
+export default class NavigationBar extends Vue { }
+</script>
---
+++ src/components/layout/NavigationMenu/NavigationMenu.vue
@@ -0,0 +1,15 @@
+<template>
+  <NavigationBar></NavigationBar>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import NavigationBar from '@/components/layout/NavigationMenu/NavigationBar.vue'
+
+@Options({
+  components: {
+    NavigationBar
+  },
+})
+export default class NavigationMenu extends Vue { }
+</script>
